e2fc726bb4cd8b3c221a57367addd01b16081fe3
booth-w <personal@booth-w.co.uk>
2024-12-10 19:01:35 +0000

    Add day 1 part 2

--- 01/main.py
+++ 01/main.py
@@ -1,8 +1,16 @@
+import collections
+
+
 def part1(input: list) -> int:
 	left, right = zip(*[line.split() for line in input])
 	return sum(abs(int(l) - int(r)) for l, r in zip(sorted(left), sorted(right)))
 
+def part2(input: list) -> int:
+	left, right = zip(*[line.split() for line in input])
+	rightCounter = collections.Counter(right)
+	return sum([rightCounter[l]*int(l) for l in left])
+
 
 if __name__ == "__main__":
 	with open("input.txt") as f:
-		print(part1(f.readlines()))
+		print(part2(f.readlines()))
